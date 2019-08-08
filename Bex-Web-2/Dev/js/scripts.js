f0e3920b598331bf4f72694ee04f0d64fa8b225b
Bex <bexlewis05@gmail.com>
2019-08-07 20:35:45 -0400

    Final Push - Works

--- Bex-Web-2/Dev/js/scripts.js
+++ Bex-Web-2/Dev/js/scripts.js
@@ -1,5 +1,4 @@
-var $burger=$("#burger-icon"),$nav=$("nav[data-nav]"),$navA=$("nav[data-nav] a"),isVisible=!1;function mouseClick(){console.log("click"),!1===isVisible?($nav.show(),isVisible=!0):($nav.hide(),isVisible=!1)}$burger.on("click",mouseClick),$navA.on("click",mouseClick);
-
+var $burger=$("#burger-container"),$nav=$("nav[data-nav]"),$navA=$("nav[data-nav] a"),isVisible=!1;function mouseClick(){console.log("click"),!1===isVisible?($nav.show(),isVisible=!0):($nav.hide(),isVisible=!1)}$burger.on("click",mouseClick),$navA.on("click",mouseClick);
 
 var $topLine = $('#burger-container div:nth-child(1)');
 var $middleLine = $('#burger-container div:nth-child(2)');
